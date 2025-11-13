8307bf9d9a928213c72420c6fb3a8393770027d0
Alexx1257 <alejandromacias1257@gmail.com>
2025-11-13 14:03:27 -0600

    four commit

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom' // ⬅️ Importar Link
 import { formatPrice } from '../../utils/formatters'
-import { getPriceWithDiscount } from '../../data/Products'
 import './ProductCard.css'
 
 function ProductCard({ 
